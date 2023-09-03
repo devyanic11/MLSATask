6dc269cd09f9f8c3d1d53a62d45076ff68942179
devyanic11 <104618068+devyanic11@users.noreply.github.com>
2023-09-03 16:28:25 +0530

    add changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/des.jpeg";
+import image from "../images/desk.jpeg";
 
 const imageAltText = "desk with pen, notes, books and laptop";
 
